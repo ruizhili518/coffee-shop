a4ad6c52ed4352c1a40bd6458a71b0d24529c83e
ruizhili518 <ruizhili518@gmail.com>
2024-11-18 15:36:01 -0800

    Test backend deplyment CORS issue.

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const corsOptions = {
         if (allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
-            callback(new Error('Not allowed by CORS'));
+            callback(new Error(`Not allowed by CORS: ${origin}`));
         }
     },
     credentials: true
